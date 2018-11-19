93955e63ea041f500d54abbd3c87a5b6e03313e9
pascal_rueegger <pascalrueegger@gmx.ch>
2018-11-19 15:32:58 +0100

    trying to use styled components and failing

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import './main.css';
+import styled from 'styled-components';
+// import './main.css';
+
+const Body = styled.div`
+  font-family: sans-serif;
+  font-weight: 600;
+  color: white;
+  text-align: center;
+  background-color: rgb(9, 45, 65);
+  height: 600px;
+  margin: 0;
+  padding: 20px;
+`
+
 
 export default class App extends React.Component {
   render() {
-    return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+    return (
+      <Body>
+        <h2>Welcome to styled React!</h2>
+      </Body>
+    );
   }
 }
+
+// <div style={ mainStyle } >
+//   <h2>Welcome to React!</h2>
+// </div>
