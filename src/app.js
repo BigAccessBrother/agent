6b1b03ec48318a2b0377970f293aba113512d0ef
pascalrueegger <pascalrueegger@gmx.ch>
2018-11-19 22:06:01 +0100

    minor cleanup

--- src/app.js
+++ src/app.js
@@ -16,39 +16,13 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    // exec(
-    //   'powershell -command "Get-MpComputerStatus"',
-    //   (err, stdout, stderr) => {
-    //     console.log(1, stdout);
-    //     let arr = stdout.split('\n').filter(info => info.length > 2);
-    //     console.log(2, arr)
-    //     this.setState({
-    //       test: arr
-    //     })
-    //   }
-    // );
     getWindows();
   }
-
-  // test() {
-  //   exec(
-  //     'powershell -command "Get-MpComputerStatus"',
-  //     (err, stdout, stderr) => {
-  //       console.log(1, stdout);
-  //       let arr = stdout.split('\n');
-  //       console.log(2, arr)
-  //       this.setState({
-  //         test: arr
-  //       })
-  //     }
-  //   );
-  // }
 
   render() {
     return (
       <div style={ mainStyle } >
           <h2>BAB status report:</h2>
-          {/* <button onClick={ this.test.bind(this) } >test</button> */}
           <ul style={ text } >
           { this.state.test.map(
               info => <li key={ info }>{ info }</li> 
