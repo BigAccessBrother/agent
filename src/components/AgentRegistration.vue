abc9e656bef7a1ba876b67df22782822706aa05a
pascalrueegger <pascalrueegger@gmx.ch>
2018-11-23 09:48:03 +0100

    registration request in place

--- src/components/AgentRegistration.vue
+++ src/components/AgentRegistration.vue
@@ -2,13 +2,45 @@
     <div>
         <div>One time registration.</div>
         <div>Please enter your email and password</div>
-        <input type="text" placeholder="email">
-        <input type="password" placeholder="password">
+        <input type="text" placeholder="email" v-model="email">
+        <input type="password" placeholder="password" v-model="password">
+        <button v-on:click="register">register agent</button>
+        <div class="AgentRegistration-error">{{ message }}</div>
     </div>
 </template>
 
 <script>
+    import { makeRequest } from '../utils/requests';
+
     export default {
-        name: 'agent-registration'
+        name: 'agent-registration',
+        props: ['number'],
+        data () {
+            return {
+                message: ''
+            }
+        },
+        methods: {
+            register () {
+                const body = {
+                    email: this.email,
+                    password: this.password,
+                    system_serial_number: this.number
+                }
+                console.log('register body: ', body);
+                makeRequest('/api/agent/register/asdasd', 'POST', body)
+                .then(response => {
+                        if (response.ok) {
+                            this.$emit('register-agent')
+                        } else {
+                            this.email = '';
+                            this.password = '';
+                            this.message = `registration failed (${response.status})`;
+                        }
+                    }    
+                )
+            }
+        }
+
     }
 </script>
